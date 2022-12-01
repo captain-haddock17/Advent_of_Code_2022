e947165e31bcfc4616342e997832e1662846a764
William J. Franck <william@sterna.io>
2022-12-01 13:25:08 +0100

    introduicing Group_of_Elfs; Elfs_with_Most_Calories

--- puzzle_01/src/puzzle_01.adb
+++ puzzle_01/src/puzzle_01.adb
@@ -32,9 +32,39 @@ procedure Puzzle_01 is
       Total_Food : Calories;
    end record;
 
+   --  Rank of Elfs, with position 1 = Highest Calories value recorded
+   subtype Group_Rank is Positive range 1 .. 1;
+   type Group_of_Elfs is array (Group_Rank) of Elf;
+
+   Some_Elf : Elf := (Name => 0, Total_Food => 0);
+   Elfs_with_most_Food : Group_of_Elfs :=
+      (others => (Name => 0, Total_Food => 0));
+
    Elf_Index : Natural := 0;
    Some_Food : Calories := 0;
-   Some_Elf, Elf_with_most_Food : Elf := (Name => 0, Total_Food => 0);
+
+   function Elfs_with_Most_Calories (
+      Some_Elf : Elf;
+      Top_Elfs : Group_of_Elfs)
+      return Group_of_Elfs is
+
+      New_Top_Elfs : Group_of_Elfs := Top_Elfs;
+   begin
+      for One_of in Group_Rank loop
+         if Some_Elf.Total_Food > Top_Elfs (One_of).Total_Food then
+            --  first retrograde the other elfs in the group
+            for Next in reverse One_of + 1 .. Group_Rank'Last loop
+               New_Top_Elfs (Next).Total_Food := Top_Elfs (Next - 1).Total_Food;
+               New_Top_Elfs (Next).Name := Top_Elfs (Next - 1).Name;
+            end loop;
+            --  now replace with new value
+            New_Top_Elfs (One_of).Total_Food := Some_Elf.Total_Food;
+            New_Top_Elfs (One_of).Name := Some_Elf.Name;
+         end if;
+      end loop;
+
+      return New_Top_Elfs;
+   end Elfs_with_Most_Calories;
 
    --  ----------------------------
    --  File and Run-Time Parameters
@@ -82,10 +112,9 @@ begin
          --  Some_Elf.Name := Elf_Index;
          Some_Elf.Total_Food := @ + Some_Food;
       else
-         if Some_Elf.Total_Food > Elf_with_most_Food.Total_Food then
-            Elf_with_most_Food.Total_Food := Some_Elf.Total_Food;
-            Elf_with_most_Food.Name := Elf_Index;
-         end if;
+         Elfs_with_most_Food := Elfs_with_Most_Calories (
+            Some_Elf => Some_Elf,
+            Top_Elfs => Elfs_with_most_Food);
          Next_Elf := True;
          Some_Elf.Total_Food := 0;
       end if;
@@ -95,12 +124,12 @@ begin
    New_Line;
    if Elf_Index > 0 then
       Put_Line ("Total Calories of Elf_with_most_Food ="
-         & Calories'Image (Elf_with_most_Food.Total_Food));
+         & Calories'Image (Elfs_with_most_Food (Group_Rank'First).Total_Food));
    else
       Put_Line ("No Elfs found in file ?!?!");
    end if;
 
-   if Elf_with_most_Food.Total_Food = 71_502 then
+   if Elfs_with_most_Food (Group_Rank'First).Total_Food = 71_502 then
       Put_Line ("Correct answer ;-)");
    end if;
    New_Line;
