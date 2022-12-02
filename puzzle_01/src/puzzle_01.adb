940525c55dbece55c1398a05216ab66d786f85c1
William J. Franck <william@sterna.io>
2022-12-02 12:15:17 +0100

    SPDX license bloc in header

--- puzzle_01/src/puzzle_01.adb
+++ puzzle_01/src/puzzle_01.adb
@@ -1,11 +1,10 @@
 --  -------------------------------------------------------------
---  Author : William J. FRANCK
---  e-Mail : william@sterna.io
---
---  Initial creation date : 2022-12-01
---  -------------------------------------------------------------
---  License : CC-BY-SA
+--  SPDX-License-Identifier: CC-BY-SA-4.0
+--  SPDX-FileCopyrightText: Copyright 2022 William J. FRANCK (william@sterna.io)
+--  SPDX-Creator: William J. FRANCK (william@sterna.io)
 --  Ref: https://creativecommons.org/licenses/by-sa/4.0/legalcode
+--  -------------------------------------------------------------
+--  Initial creation date : 2022-12-01
 --  -------------------------------------------------------------
 pragma Ada_2022;
 pragma Style_Checks ("M120");
