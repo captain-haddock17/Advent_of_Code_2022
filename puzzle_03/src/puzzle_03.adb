e75fbab4077019d486f6cb42cf41750e29da876f
William J. Franck <william@sterna.io>
2022-12-03 10:59:44 +0100

    use a loop for Intersection of Grouped_Rucksack

--- puzzle_03/src/puzzle_03.adb
+++ puzzle_03/src/puzzle_03.adb
@@ -1,3 +1,4 @@
+pragma Style_Checks ("M120");
 --  -------------------------------------------------------------
 --  SPDX-License-Identifier: CC-BY-SA-4.0
 --  SPDX-FileCopyrightText: Copyright 2022 William J. FRANCK (william@sterna.io)
@@ -7,7 +8,6 @@
 --  Initial creation date : 2022-12-03
 --  -------------------------------------------------------------
 pragma Ada_2022;
--- pragma Style_Checks ("M120");
 
 with Rucksacks;
 use Rucksacks;
@@ -23,23 +23,24 @@ procedure Puzzle_03 is
 
    Run_Args    : Command_Line.Program_args;
 
-   Total_Priorities_Part1 : Natural := 0;
-
    --  File and Run-Time Parameters
    --  ----------------------------
    Data_File : Ada.Text_IO.File_Type;
 
    --  Part1
-   Some_Rucksack : Rucksack 
+   Some_Rucksack : Rucksack
       := (others => Rucksack_Compartment.Empty_Set);
    Common_Item : Item_Type;
+   Total_Priorities_Part1 : Natural := 0;
 
    --  Part2
+   use Rucksack_Compartment;
+   Badge_Item : Item_Type;
    Priority,
-   Total_Priorities_Part2 
+   Total_Priorities_Part2
       : Natural := 0;
-   Grouped_Rucksack : Grouped_Rucksack_array 
-      := (others => Rucksack_Compartment.Empty_Set);
+   Grouped_Rucksack : Grouped_Rucksack_array
+      := (others => Empty_Set);
    Rucksack_Index_in_Group : Positive := 1;
 
 -- -----
@@ -62,51 +63,59 @@ begin
          --  Part1
          Compartment_Items_Count : Natural;
       begin
-          Compartment_items_Count := Rucksack_Items_Count / 2;
-          -- Load an empty rucksack
-         Some_Rucksack := (others => Rucksack_Compartment.Empty_Set);
-          for I in 1 .. Compartment_Items_Count loop
+         Compartment_Items_Count := Rucksack_Items_Count / 2;
+         --  Load an empty rucksack
+         Some_Rucksack := (others => Empty_Set);
+         for I in 1 .. Compartment_Items_Count loop
             Add (Into_Rucksack => Some_Rucksack (1), Item => Some_Rucksack_Data (I));
          end loop;
-          for I in Compartment_Items_Count + 1 .. Rucksack_Items_Count loop
+         for I in Compartment_Items_Count + 1 .. Rucksack_Items_Count loop
             Add (Into_Rucksack => Some_Rucksack (2), Item => Some_Rucksack_Data (I));
          end loop;
 
-          -- Find a common item in this rucksack
+         --  Find a common item in this rucksack
          Common_Item := Find_First_Common (Some_Rucksack (1), Some_Rucksack (2));
-
-          -- Sum-up the priority of that item
-          Total_Priorities_Part1 := @ + Priority_of (Common_Item);
-
-         --  Part2
-         Rucksack_Compartment.Union (
-            Target => Grouped_Rucksack (Rucksack_Index_in_Group), 
-            Source => Some_Rucksack (1));
-         Rucksack_Compartment.Union (
-            Target => Grouped_Rucksack (Rucksack_Index_in_Group), 
-            Source => Some_Rucksack (2));
-
-      exception
-         when Constraint_Error =>
-            Text_IO.Put (Standard_Error, 
-               "Total number of items in a rucksack is not even!");
-      end;
-
-      if Rucksack_Index_in_Group = 3 then
-         Common_Item := Find_First_Common (Grouped_Rucksack (1), 
-            Rucksack_Compartment.Intersection (
-               Grouped_Rucksack (2), 
-               Grouped_Rucksack (3)));
-
          Priority := Priority_of (Common_Item);
+
          if Run_Args.Trace then -- Trace
             Put_Line ("   Priority (" & Common_Item & ") =" & Priority'Image);
          end if;
 
+         --  Sum-up the priority of that item
+         Total_Priorities_Part1 := @ + Priority;
+
+         --  Part2
+         Union (
+            Target => Grouped_Rucksack (Rucksack_Index_in_Group),
+            Source => Some_Rucksack (1));
+         Union (
+            Target => Grouped_Rucksack (Rucksack_Index_in_Group),
+            Source => Some_Rucksack (2));
+
+      exception
+         when Constraint_Error =>
+            Text_IO.Put (Standard_Error,
+               "Total number of items in a rucksack is not even!");
+      end;
+
+      --  Find a common badge/priority item in grouped rucksacks
+      if Rucksack_Index_in_Group = Group_range'Last then
+         for i in Group_range'First + 1 .. Group_range'Last loop
+            Intersection (
+               Grouped_Rucksack (Group_range'First),
+               Grouped_Rucksack (i));
+         end loop;
+         Badge_Item := Find_First_Common (Grouped_Rucksack (Group_range'First));
+         Priority := Priority_of (Badge_Item);
+
+         if Run_Args.Trace then -- Trace
+            Put_Line ("   Group (" & Badge_Item & ") =" & Priority'Image);
+         end if;
+
          Total_Priorities_Part2 := @ + Priority;
 
-         Rucksack_Index_in_Group := 1;
-         Grouped_Rucksack := (others => Rucksack_Compartment.Empty_Set);
+         Rucksack_Index_in_Group := Group_range'First;
+         Grouped_Rucksack := (others => Empty_Set);
       else
          Rucksack_Index_in_Group := @ + 1;
       end if;
@@ -116,7 +125,7 @@ begin
 
    --  Print result of Part 1
    Put ("Total Priorities  (Part 1) =");
-   put_Line (Total_Priorities_Part1'Image);
+   Put_Line (Total_Priorities_Part1'Image);
 
    if Total_Priorities_Part1 = 157 then
       Put_Line ("   Correct answer with test data ;-)");
@@ -129,7 +138,7 @@ begin
 
    --  Print result of Part 2
    Put ("Total Priorities (Part 2) =");
-   put_Line (Total_Priorities_Part2'Image);
+   Put_Line (Total_Priorities_Part2'Image);
 
    if Total_Priorities_Part2 = 70 then
       Put_Line ("   Correct answer with test data ;-)");
